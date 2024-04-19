872d712523314c30d4b3c067193bdf3824f7124e
nalli narayanan <nalli@gmail.com>
2024-04-19 20:55:00 +0530

    changd added

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,36 +1,37 @@
-import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
-import { GameService } from "../../services/game.service";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GameService } from '../../services/game.service';
+import { ToastrService } from 'ngx-toastr';
 @Component({
-  selector: "app-game",
-  templateUrl: "./game.component.html",
-  styleUrls: ["./game.component.scss"],
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss'],
 })
 export class GameComponent {
-  title = "Game Component";
+  title = 'Game Component';
   roomId: string | null = null;
   players: any = [];
   cards = [
-    { isFlipped: false, role: "raja",selected:false },
-    { isFlipped: false, role: "rani",selected:false },
-    { isFlipped: false, role: "theif",selected:false },
-    { isFlipped: false, role: "police",selected:false },
+    { id: 1, isFlipped: false, role: 'raja', selected: false },
+    { id: 2, isFlipped: false, role: 'rani', selected: false },
+    { id: 3, isFlipped: false, role: 'theif', selected: false },
+    { id: 4, isFlipped: false, role: 'police', selected: false },
   ];
-  currentPlayerId!:number;
+  currentPlayerId!: number;
+  currentPlayer: any;
 
   constructor(
     private route: ActivatedRoute,
-    private myService: GameService // Inject your service
+    private myService: GameService,
+    private toastr: ToastrService
   ) {}
 
-
   ngOnInit() {
-      this.currentPlayerId = Number(sessionStorage.getItem('playerId'));
-
+    this.currentPlayerId = Number(sessionStorage.getItem('playerId'));
     this.myService.connect();
     // Subscribe to the route parameters
     this.route.paramMap.subscribe((params) => {
-      this.roomId = params.get("id"); // Get the room ID from the URL
+      this.roomId = params.get('id'); // Get the room ID from the URL
       if (this.roomId) {
         // Fetch the list of players
         this.fetchPlayers(this.roomId);
@@ -40,40 +41,105 @@ export class GameComponent {
 
   // Fetch the list of players
   fetchPlayers(roomId: string) {
-    console.log("eeeeee", roomId);
+    console.log('eeeeee', roomId);
     this.myService.getPlayersByRoomId(roomId).subscribe(
       (data) => {
-        console.log(data, "com");
+        console.log(data, 'com');
         // Update the players array with data from the backend
         this.players = data;
+        this.currentPlayer = this.players.find(
+          (c: any) => c.playerId === this.currentPlayerId
+        );
       },
       (error) => {
         // Handle any errors here
-        console.error("Error fetching players:", error);
+        console.error('Error fetching players:', error);
       }
     );
   }
 
-toggleCard(card: any): void {
-  card.selected = true;
-    console.log(card,this.players,this.currentPlayerId);
+  async toggleCard(card: any): Promise<void> {
+    this.currentPlayer = this.players.find(
+      (c: any) => c.playerId === this.currentPlayerId
+    );
 
-    this.myService.updateCardDetails(card,this.currentPlayerId,this.roomId);
-    console.log(this.myService.getUpdatedCardDetails());
-    this.myService.getUpdatedCardDetails().subscribe((players:any)=>{
-      console.log(players);
-      // let player = players.filter((x:any) => x.playerId === this.currentPlayerId).pop();
-      // console.log(player);
-      // if(player.selected && player.role === card.role){
+    let assignedRole = this.players.filter((p: any) => p.role === card.role);
 
-      // }
-    })
+    let playersUpdated = false;
+
+    console.log(assignedRole, this.currentPlayer);
+
+    if (
+      assignedRole.length > 0 &&
+      assignedRole[0].playerId !== this.currentPlayerId
+    ) {
+      this.toastr.warning('This card is already taken by someone');
+      return;
+    }
+
+    if (this.currentPlayer?.selected) {
+      if (card.role === this.currentPlayer.role) {
+        card.isFlipped = !card.isFlipped;
+      }
+      this.toastr.warning(
+        "Once card is locked and you can't select again",
+        'You are already selected the card'
+      );
+      return;
+    }
+
+    console.log('Contine');
+
+    try {
+      await this.myService.updateCardDetails(
+        card,
+        this.currentPlayerId,
+        this.roomId
+      );
+
+      this.myService.getUpdatedCardDetails().subscribe((players: any) => {
+        this.players = players;
+        console.log('Updated players inside subscription:', this.players);
+        let currentPlayer = players.find(
+          (c: any) => c.playerId === this.currentPlayerId
+        );
+        console.log(currentPlayer);
+        if (currentPlayer?.selected) {
+          if (card.role === currentPlayer.role) {
+            card.isFlipped = !card.isFlipped;
+          }
+        }
+
+        console.log(
+          players.filter(
+            (x: any) =>
+              x.role === card.role && x.playerId != this.currentPlayerId
+          )
+        );
+
+        playersUpdated = true;
+        // Use the updated players list here
+      });
+
+      // this.players = [...players];
+      // console.log(this.players);
+    } catch (error) {
+      console.error('Error occurred:', error);
+    }
+
+    setTimeout(() => {
+      if (playersUpdated) {
+        console.log('Updated players outside subscription:', this.players);
+      }
+    }, 2);
+
+    console.log(this.players);
+
     // card.isFlipped = !card.isFlipped; // Toggle the isFlipped property
   }
 
-
   shuffle() {
-    const cardContainer = document.getElementById("cardContainer");
+    const cardContainer = document.getElementById('cardContainer');
     if (cardContainer) {
       const cards = cardContainer.children;
       console.log(cards);
@@ -89,20 +155,19 @@ toggleCard(card: any): void {
 
       // Add a delay between each card movement for animation effect
       cardArray.forEach(function (card, index) {
-        if(card instanceof HTMLDivElement){
+        if (card instanceof HTMLDivElement) {
           setTimeout(function () {
-            card.style.transform = "translateY(-100%)";
+            card.style.transform = 'translateY(-100%)';
             card.classList.add(hiddenClass);
             setTimeout(function () {
               cardContainer.appendChild(card);
               setTimeout(function () {
-                card.style.transform = "translateY(0)";
+                card.style.transform = 'translateY(0)';
                 card.classList.remove(hiddenClass);
               }, 10);
             }, 500); // This delay should match the transition duration in CSS
           }, index * 100); // Delay each card by 100ms
         }
-
       });
     }
   }
--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from "@angular/core";
-import { environment } from "src/environments/environment";
-import { io, Socket } from "socket.io-client";
-import { observable, Observable } from "rxjs";
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { io, Socket } from 'socket.io-client';
+import { observable, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class GameService {
   socket: Socket | undefined;
@@ -15,17 +15,17 @@ export class GameService {
   }
 
   createRoom(obj: any) {
-    this.socket?.emit("createroom", obj);
+    this.socket?.emit('createroom', obj);
     this.getRoomId();
   }
 
   joinRoom(playerDetails: any) {
-    this.socket?.emit("joinroom", playerDetails);
+    this.socket?.emit('joinroom', playerDetails);
   }
 
   getJoinedPlayers() {
     return new Observable((observer) => {
-      this.socket?.on("playersList", (players) => {
+      this.socket?.on('playersList', (players) => {
         observer.next(players);
       });
     });
@@ -33,7 +33,7 @@ export class GameService {
 
   getRoomId() {
     return new Observable((observer) => {
-      this.socket?.on("roomCreated", (roomId) => {
+      this.socket?.on('roomCreated', (roomId) => {
         observer.next(roomId);
       });
     });
@@ -41,33 +41,33 @@ export class GameService {
 
   getJoinedRoomId() {
     return new Observable((observer) => {
-      this.socket?.on("roomid", (roomId) => {
+      this.socket?.on('roomid', (roomId) => {
         observer.next(roomId);
       });
     });
   }
 
   getJoinedNotification() {
-    this.socket?.on("playerJoinedNotification", (message) => {
+    this.socket?.on('playerJoinedNotification', (message) => {
       console.log(message);
     });
   }
   roomFull() {
     return new Observable((observer) => {
-      this.socket?.on("roomFull", (data) => {
+      this.socket?.on('roomFull', (data) => {
         observer.next(data);
       });
     });
   }
 
   playUpdate(obj: any) {
-    console.log(obj, "sssssssssssssssss");
-    this.socket?.emit("playUpdate", obj);
+    console.log(obj, 'sssssssssssssssss');
+    this.socket?.emit('playUpdate', obj);
   }
 
   roomMem() {
     return new Observable((observer) => {
-      this.socket?.on("roomMem", (data) => {
+      this.socket?.on('roomMem', (data) => {
         observer.next(data);
       });
     });
@@ -77,31 +77,47 @@ export class GameService {
     return new Observable<any[]>((observer) => {
       // Function to handle the list of players received from the server
       const handlePlayersList = (playersList: any[]) => {
-        console.log("Received players list:", playersList);
+        console.log('Received players list:', playersList);
         observer.next(playersList);
         observer.complete();
       };
 
       // Listen for the 'playerList' event from the server
-      this.socket?.on("playerList", handlePlayersList);
-      console.log("cccccccccccccc", roomID);
+      this.socket?.on('playerList', handlePlayersList);
+      console.log('cccccccccccccc', roomID);
       // Emit a request to the server to get players in the specified room
-      this.socket?.emit("getPlayers", roomID);
+      this.socket?.emit('getPlayers', roomID);
 
       // Clean up the subscription when the observable is unsubscribed
       return () => {
-        this.socket?.off("playerList", handlePlayersList);
+        this.socket?.off('playerList', handlePlayersList);
       };
     });
   }
 
   updateCardDetails(card: any, playerid: any, roomid: any) {
-    this.socket?.emit("selectCard", card, playerid, roomid);
+    this.socket?.emit('selectCard', card, playerid, roomid);
   }
 
   getUpdatedCardDetails() {
     return new Observable((observer) => {
-      this.socket?.on("updatedPlayerListCard", (data) => {
+      this.socket?.on('updatedPlayerListCard', (data) => {
+        observer.next(data);
+      });
+    });
+  }
+
+  checkAssignedRole() {
+    return new Observable((observer) => {
+      this.socket?.on('isAssignedRole', (data) => {
+        observer.next(data);
+      });
+    });
+  }
+
+  checkCurrentPlayer() {
+    return new Observable((observer) => {
+      this.socket?.on('isCurrentPlayer', (data) => {
         observer.next(data);
       });
     });
